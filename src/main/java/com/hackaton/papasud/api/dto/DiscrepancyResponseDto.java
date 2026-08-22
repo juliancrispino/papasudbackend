d9f3f80b9f40c2cd1562f449804c5bc61327fc66
Julian pc2 <crispinojulian@hotmail.com>
2026-08-22 14:55:39 -0300

    ia3

--- src/main/java/com/hackaton/papasud/api/dto/DiscrepancyResponseDto.java
+++ src/main/java/com/hackaton/papasud/api/dto/DiscrepancyResponseDto.java
@@ -1,16 +1,22 @@
 package com.hackaton.papasud.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DiscrepancyResponseDto {
     private DiscrepancyAnalysisDto data;
     private String error;
 
     @Data
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class DiscrepancyAnalysisDto {
         private String engine; // 'llm' or 'heuristic'
         private String explanation;
@@ -23,6 +29,8 @@ public class DiscrepancyResponseDto {
 
     @Data
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Evidence {
         private String type; // 'movement' or 'other'
         private String reference;
