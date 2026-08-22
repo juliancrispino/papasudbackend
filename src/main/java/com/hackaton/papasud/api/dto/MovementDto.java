a8a9ee759e3e53d5403a49f2080de7b7f1ac55c7
Julian pc2 <crispinojulian@hotmail.com>
2026-08-22 14:36:56 -0300

    movement

--- src/main/java/com/hackaton/papasud/api/dto/MovementDto.java
+++ src/main/java/com/hackaton/papasud/api/dto/MovementDto.java
@@ -1,11 +1,15 @@
 package com.hackaton.papasud.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MovementDto {
     private String id;
     private String lotId;
